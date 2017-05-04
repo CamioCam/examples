79fd309cb12856958a3ac48c100880bc254cdeaa
John Allard <john@jhallard.com>
2017-05-04 12:18:14 -0700

    Added missing PIL dependency to hook-example

--- hooks/hook-install.sh
+++ hooks/hook-install.sh
@@ -1,5 +1,6 @@
 sudo apt-get install mongodb
 sudo apt-get install python-dev
+sudo apt-get install python-pil
 pip install bottle gunicorn pymongo requests
 nohup gunicorn -w 2 app:hook-example -b 0.0.0.0 > /tmp/gunicorn.log &
 nohup python hook-example.py > /tmp/taskqueue.log &
