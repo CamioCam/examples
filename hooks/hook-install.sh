ad650c19038844bf34501d2312ec24fa3f100fd1
John Allard <john@jhallard.com>
2017-05-04 19:00:04 -0700

    Fixed the gunicorn command in hook-install

--- hooks/hook-install.sh
+++ hooks/hook-install.sh
@@ -1,6 +1,8 @@
+#!/usr/bin/env bash
+
 sudo apt-get install mongodb
 sudo apt-get install python-dev
 sudo apt-get install python-pil
 pip install bottle gunicorn pymongo requests
-nohup gunicorn -w 2 app:hook-example -b 0.0.0.0 > /tmp/gunicorn.log &
+nohup gunicorn -w 2 -b 0.0.0.0:80 hook-example:app > /tmp/gunicorn.log &
 nohup python hook-example.py > /tmp/taskqueue.log &
